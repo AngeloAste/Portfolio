f90002b26a325c0e0f6e28b0affbf62ba6274e4e
Angelo Aste <ext.angelo.aste@uss.cl>
2025-03-03 13:54:39 +0000

    fix skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,10 @@ import html from "../icons/html.png";
 import react from "../icons/react.png";
 import js from "../icons/js.png";
 import ex from "../icons/ex.png";
+import java from "../icons/java.png";
+import oracle from "../icons/oracleDB.png";
+import net from "../icons/dot_net.png";
+import netCore from "../icons/netCore.png";
 import node from "../icons/node.png";
 import db from "../icons/db.png";
 import sql from "../icons/sql.png";
@@ -22,11 +26,15 @@ const About = () => {
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={css}/>CSS</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={js}/>JavaScript</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={react}/>Reactjs</button>
-            <br/>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={node}/>Node</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={ex}/>Express</button>
+            <br/>
+            <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={net}/>.NET</button>
+            <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={netCore}/>.NET Core</button>
+            <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={java}/>Java</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={db}/>Mongodb</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={sql}/>SQL</button>
+            <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={oracle}/>Oracle</button>
             <button className="btn btn-dark"><img style={{height:'20px',paddingRight:'10px'}}  src={bootstrap}/>Bootstrap</button>
         </div>
     </div>
